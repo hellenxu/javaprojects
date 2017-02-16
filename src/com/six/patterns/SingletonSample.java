99bb616f6f29b76ab9ba93966232458364d447f3
hellenxu <heavensxu@gmail.com>
2017-02-16 15:06:34 -0500

    singleton sample v1

---
+++ src/com/six/patterns/SingletonSample.java
@@ -0,0 +1,24 @@
+package com.six.patterns;
+
+/**
+ * Example of Singleton Pattern
+ * Created by hellenxu on 2017-02-16.
+ */
+public class SingletonSample {
+    private static SingletonSample instance;
+
+    public static SingletonSample getInstance(){
+        if(instance == null){
+            synchronized (SingletonSample.class){
+                if (instance == null){
+                    instance = new SingletonSample();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private SingletonSample(){
+        // initialization
+    }
+}
