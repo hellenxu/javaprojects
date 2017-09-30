d5d778ff16b8a4a20793a6b5dc6556d0a411a83d
hellenxu <heavensxu@gmail.com>
2017-09-29 22:25:04 -0400

    Add: big o of bucket sort.

---
+++ src/com/six/algorithm/AlgorithmDemo.java
@@ -0,0 +1,9 @@
+package com.six.algorithm;
+
+public class AlgorithmDemo {
+
+    public static void main(String[] args) {
+        BucketSort.bucketSort(10, 6);
+
+    }
+}
--- src/com/six/algorithm/BucketSort.java
+++ src/com/six/algorithm/BucketSort.java
@@ -5,31 +5,32 @@ import java.util.Scanner;
 /**
  * 10 scores: 3, 5, 7, 1, 0, 10, 8, 9, 6, 7
  * Requirements: output scores from high to low
+ * Big O: O(2*(M + N)) ≈ O(M + N)
+ * M: the amount of buckets
+ * N: the amount of numbers
  * <p>
  * Created by Xiaolin on 2017-09-28.
  */
 
 public class BucketSort {
 
-    public static void main(String[] args) {
-        //initialization
-        int[] scores = new int[11];
-        int size = 11;
-        for (int i = 0; i < size; i++) {
+    static void bucketSort(int buckets, int numbers) {
+        int[] scores = new int[buckets];
+        for (int i = 0; i < buckets; i++) {
             scores[i] = 0;
         }
 
-        System.out.println("Please input 10 numbers that not larger than 10.");
+        System.out.printf("Please input %d numbers that not larger than %d: ", numbers, buckets - 1);
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < numbers; i++) {
             ++scores[scanner.nextInt()];
         }
 
-        //show results
-        for (int i = size - 1; i > 0; i--) {
+        for (int i = buckets - 1; i > 0; i--) {
             for (int j = 1; j <= scores[i]; j++) {
-                System.out.printf("%d ; ", i);
+                System.out.printf("%d; ", i);
             }
         }
+
     }
 }
