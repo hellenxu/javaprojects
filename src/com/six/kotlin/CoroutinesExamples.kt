ea2ce1ff495150f6de66de5d7ef7fc0311119894
hellenxu <heavensxu@gmail.com>
2018-07-27 12:27:45 -0400

    Add: runBlocking example.

--- src/com/six/kotlin/CoroutinesExamples.kt
+++ src/com/six/kotlin/CoroutinesExamples.kt
@@ -3,20 +3,31 @@ package com.six.kotlin
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 
-fun main(args: Array<String>) {
+//fun main(args: Array<String>) {
+//
+//    /**
+//     * 1) main thread created;
+//     * 2) one coroutine created and delayed 1s;
+//     * 3) Hello is printed;
+//     * 4) After 1s, World! is printed.
+//     */
+//    launch(CommonPool) {
+//        delay(1000L)
+//        println("World!")
+//    }
+//
+//    print("Hello ")
+//    Thread.sleep(2000L)
+//}
 
-    /**
-     * 1) main thread created;
-     * 2) one coroutine created and delayed 1s;
-     * 3) Hello is printed;
-     * 4) After 1s, World! is printed.
-     */
+fun main(args: Array<String>) = runBlocking<Unit> {
     launch(CommonPool) {
         delay(1000L)
         println("World!")
     }
 
     print("Hello ")
-    Thread.sleep(2000L)
+    delay(2000L)
 }
