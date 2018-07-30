d164cf6a873f38759eef8b334a30c3a74c30586a
hellenxu <heavensxu@gmail.com>
2018-07-29 21:39:30 -0400

    Add: non-cancellable coroutines example.

--- src/com/six/kotlin/CoroutinesExamples.kt
+++ src/com/six/kotlin/CoroutinesExamples.kt
@@ -71,13 +71,36 @@ import kotlinx.coroutines.experimental.runBlocking
 /**
  * cancel Coroutines example
  */
+//fun main(args: Array<String>) = runBlocking<Unit>{
+//    //以下的job不断重复，直到完成重复次数1000或者主协程结束;
+//    //主协程delay 1300L的过程中, job可以重复3次;然后job.cancel()被执行
+//    val job = launch(CommonPool) {
+//        repeat(1000) {
+//            println("I'm sleeping $it")
+//            delay(500L)
+//        }
+//    }
+//
+//    delay(1300L)
+//    println("main: I'm tired of waiting!")
+//    job.cancel()
+//    delay(1300L)
+//    println("main: Now I can quit.")
+//}
+
+/**
+ * non-cancellable coroutines example
+ */
 fun main(args: Array<String>) = runBlocking<Unit>{
-    //以下的job不断重复，直到完成重复次数1000或者主协程结束;
-    //主协程delay 1300L的过程中, job可以重复3次;然后job.cancel()被执行
     val job = launch(CommonPool) {
-        repeat(1000) {
-            println("I'm sleeping $it")
-            delay(500L)
+        var nextPrintTime = 0L
+        var i = 0
+        while(i < 10) {
+            val currentTime = System.currentTimeMillis()
+            if(currentTime >= nextPrintTime) {
+                println("I'm sleeping ${i++}")
+                nextPrintTime = currentTime + 500L
+            }
         }
     }
 
@@ -87,3 +110,4 @@ fun main(args: Array<String>) = runBlocking<Unit>{
     delay(1300L)
     println("main: Now I can quit.")
 }
+
