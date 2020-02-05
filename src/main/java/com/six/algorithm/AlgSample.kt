1c78ba0d66c53dcfe35057caad38d1f0b5790626
hellenxu <heavensxu@gmail.com>
2020-02-04 22:59:31 -0500

    check unique: solution two.

--- src/main/java/com/six/algorithm/AlgSample.kt
+++ src/main/java/com/six/algorithm/AlgSample.kt
@@ -8,19 +8,42 @@ package main.java.com.six.algorithm
 
 fun main() {
     println("xxl-check-unique-one: ${checkUniqueOne("qoqu34arouecnq")}")
+    println("xxl-check-unique-one: ${checkUniqueTwo("qoqu34arouecnq")}")
 }
 
 // solution one: nested iteration and compare
 // Big O: N*N
 fun checkUniqueOne(input: String): Boolean {
+    val start = System.currentTimeMillis()
     for (i in input.indices) {
         val tmp = input[i]
 
         for (j in i + 1 .. input.lastIndex) {
             if (tmp == input[j]) {
+                println("xxl-unique-one-cost: ${System.currentTimeMillis() - start} ms")
                 return false
             }
         }
     }
+    println("xxl-unique-one-cost: ${System.currentTimeMillis() - start} ms")
+    return true
+}
+
+// solution two: hash table
+// Big O: N
+fun checkUniqueTwo(input: String): Boolean {
+    val start = System.currentTimeMillis()
+    val tmp = hashMapOf<Int, Char>()
+    for (i in input.indices) {
+        val value = input[i]
+        val key = input[i].hashCode()
+        if (tmp.containsKey(key) && tmp[key] == value) {
+            println("xxl-unique-two-cost: ${System.currentTimeMillis() - start} ms")
+            return false
+        } else {
+            tmp[key] = value
+        }
+    }
+    println("xxl-unique-two-cost: ${System.currentTimeMillis() - start} ms")
     return true
 }
